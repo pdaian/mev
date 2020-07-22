a640b249753e66418c4137d0e0bfe0feb0130efb
Philip Daian <phil@linux.com>
2020-07-22 21:03:53 +0000

    add bound finding FINALLY OMG

---
+++ kutils.py
@@ -0,0 +1,41 @@
+from subprocess import Popen, PIPE
+import re
+
+BLANK_SPEC = """module BOUND
+    imports MEV
+    rule <k>
+            %s
+        => .
+     </k>
+    <S> .Map =>?S:Map </S>
+    <M> .Set => .Set </M>
+    <B> .List => ?A2 </B>
+    ensures %s
+endmodule
+"""
+
+def find_integer_bound(program, outfile, bound_clause):
+    bound = 99999999999999999999999999999999999999999999999999999999
+    previous_output = ""
+    output = ""
+    while True:
+        spec = BLANK_SPEC % (program, bound_clause % (bound))
+        open('/tmp/bound.k', 'w').write(spec)
+        print("Starting proof...")
+        pipe = Popen("kprove -v --debug --default-claim-type all-path --z3-impl-timeout 500 /tmp/bound.k", shell=True, stdout=PIPE, stderr=PIPE)
+        output = pipe.stdout.read() + pipe.stderr.read()
+        output = str(output, 'utf-8')
+        if "#True" in output:
+            print("BOUND FOUND!", bound)
+            print("Writing best configuration to", outfile, "...")
+            print(last_output)
+            open(outfile, 'w').write(last_output)
+            break
+        output = output[output.find('<generatedTop>'):]
+        output = output[:output.find('</generatedTop>')+15]
+        print("Found new bound", output)
+        bound = int(output.split('Uniswap in 0 |-> ')[1].split(' ')[0])
+        print("-" * 15, "\nBETTER BOUND:", bound)
+        previous_output = output
+    print("All done :)")
+
