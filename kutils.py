94735ddf0a62c18575c37438229d1946f21ee8fa
Philip Daian <phil@linux.com>
2020-07-23 23:40:36 +0000

    refactors and cleanup

--- kutils.py
+++ kutils.py
@@ -20,21 +20,24 @@ def find_integer_bound(program, outfile, bound_clause, starting_value):
     output = ""
     while True:
         spec = BLANK_SPEC % (program, bound_clause % (bound))
-        open('/tmp/bound.k', 'w').write(spec)
+        open("/tmp/bound.k", "w").write(spec)
         print("Starting proof...")
         pipe = Popen("kprove -v --debug --default-claim-type all-path --z3-impl-timeout 500 /tmp/bound.k", shell=True, stdout=PIPE, stderr=PIPE)
         output = pipe.stdout.read() + pipe.stderr.read()
-        output = str(output, 'utf-8')
+        output = str(output, "utf-8")
         if "#True" in output:
             print("BOUND FOUND!", bound)
             print("Writing best configuration to", outfile, "...")
             print(previous_output)
-            open(outfile, 'w').write(previous_output)
+            open(outfile, "w").write(previous_output)
             break
-        output = output[output.find('<generatedTop>'):]
-        output = output[:output.find('</generatedTop>')+15]
+        output = output[output.find("<generatedTop>"):]
+        output = output[:output.find("</generatedTop>")+15]
         print("Found new bound", output)
-        bound = int(output.split('Uniswap in 0 |-> ')[1].split(' ')[0])
+        if not "Uniswap in 0 |-> " in output and "<S>" in output:
+            bound = 0
+        else:
+            bound = int(output.split("Uniswap in 0 |-> ")[1].split(" ")[0])
         print("-" * 15, "\nBETTER BOUND:", bound)
         previous_output = output
     print("All done :)")
