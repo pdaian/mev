ee2eb95a333da46df6552757d35319e0da0aafc6
Kushal Babel <babel97kushal@gmail.com>
2020-09-14 16:19:25 +0000

    Modify scripts for running simulatenous experiments

--- find_mev.py
+++ find_mev.py
@@ -1,17 +1,26 @@
+import os
 import sys
 from subprocess import Popen, PIPE
 import re
+from pathlib import Path
+
+MAKER_DATA = """
+276203969674087029088707267242881295499809928317 opens vault 155042;
+276203969674087029088707267242881295499809928317 locks 9051654598458972160 collateral to vault 155042;
+276203969674087029088707267242881295499809928317 draws 800000000000000000000 debt from vault 155042;
+"""
+
 
 BLANK_SPEC = """module BOUND
     imports MEV
     rule <k>
             0 in 0 gets 0;
             0 in SAI gets 0;
-            0 bites vault %s;
             %s
+            0 bites vault %s;
          => ?X
      </k>
-    <S> ( (Uniswap in 0) |-> 2145594386963123929088 (Uniswap in SAI) |-> 289317379249580594429952 ) =>?S:Map </S>
+    <S> ( (Uniswap in 0) |-> 0 (Uniswap in SAI) |-> 0 ) =>?S:Map </S>
     <M> .Set => ?_:Set </M>
     <B> .List => ?_ </B>
     <P> .Map => ?_ </P>
@@ -20,15 +29,16 @@ BLANK_SPEC = """module BOUND
 endmodule
 """
 
-def find_mev_cdp(program, outfile, starting_value, end_value):
+def find_mev_cdp(program, spec_file, outfile, starting_value, end_value):
     global BLANK_SPEC
     for v in range(starting_value, end_value+1):
         output = ""
-        spec = BLANK_SPEC % (v, program)
-        open("bound.k", "w").write(spec)
+        spec = BLANK_SPEC % (program + MAKER_DATA, v)
+        Path(os.path.dirname(spec_file)).mkdir(parents=True, exist_ok=True)
+        open(spec_file, "w").write(spec)
         print("Starting proof..." + str(v))
         sys.stdout.flush()
-        pipe = Popen("kprove -v --debug --default-claim-type all-path --z3-impl-timeout 500 bound.k", shell=True, stdout=PIPE, stderr=PIPE)
+        pipe = Popen("kprove -v --debug --default-claim-type all-path --z3-impl-timeout 500 " + spec_file, shell=True, stdout=PIPE, stderr=PIPE)
         output = pipe.stdout.read() + pipe.stderr.read()
         output = str(output, "utf-8")
         print(output)
@@ -39,8 +49,10 @@ def find_mev_cdp(program, outfile, starting_value, end_value):
             return
 
 def main():
-    PROGRAM = open(sys.argv[1]).read()
-    find_mev_cdp(PROGRAM, "maker_mev.out", 155042, 155042)
+    PROGRAM = open('data/' + sys.argv[1]).read()
+    spec_file = sys.argv[1]+'/bound.k'
+    outfile = 'output/'+sys.argv[1]+'.out'
+    find_mev_cdp(PROGRAM, spec_file, outfile, 155042, 155042)
 
 if __name__ == '__main__':
     main()
--- process_data.py
+++ process_data.py
@@ -3,14 +3,25 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import sys
 from uniswap import Uniswap
+from copy import deepcopy
 
 fin = open(sys.argv[1], 'r')
-fout = open('actions_data_cut_'+sys.argv[2] + '_' +sys.argv[3]+'.txt', 'w')
+fout = open('data/uniswap_data_cut_'+sys.argv[2] + '_' +sys.argv[3], 'w')
 
-prices = []
+prices = {}
+lines = []
 
 start_point = False
 block_number = 0
+
+def get_price(cfg):
+    return cfg['SAI']/ cfg['0']
+
+def bootstrapped_data(f, cfg, rows):
+    f.write("Uniswap in 0 gets {:.0f};\n".format(cfg['0']))
+    f.write("Uniswap in SAI gets {:.0f};\n".format(cfg['SAI']))
+    for row in rows:
+        f.write(row)
 
 uniswap = Uniswap()
 line_num = 0
@@ -20,12 +31,19 @@ for line in fin.readlines():
     if 'block' in line:
         words = line.split()
         block_number = int(words[-1])
+    if block_number > int(sys.argv[3]):
+        break
     
+    uniswap.process(line)
+
     if block_number < int(sys.argv[2]):
-        uniswap.process(line)
-        config = uniswap.config()
+        config = deepcopy(uniswap.config()) #arghhhhh!
     elif block_number <= int(sys.argv[3]):
-        fout.write(line)
+        try:
+            prices[block_number] = get_price(uniswap.config())
+        except ZeroDivisionError:
+            pass
+        lines.append(line)
 
 '''
 for line in fin.readlines():
@@ -37,7 +55,11 @@ for line in fin.readlines():
         fout.write(line)
 '''
 
-price = config['SAI']/ config['0']
-print("Uniswap in 0", "{:.0f}".format(config['0']))
-print("Uniswap in SAI", "{:.0f}".format(config['SAI']))
-print(price)
+print("Uniswap in 0", "{:.1f}".format(config['0']))
+print("Uniswap in SAI", "{:.1f}".format(config['SAI']))
+print(get_price(config))
+
+bootstrapped_data(fout, config, lines)
+
+plt.plot(list(prices.keys()), list(prices.values()))
+plt.savefig('img/sai_prices.png')
